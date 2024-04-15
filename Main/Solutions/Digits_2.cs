9fba63a910af1b9d316ee4bad28bc2847c6b6a7d
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-15 19:49:22 +0400

    2. Digits

---
+++ Main/Solutions/Digits_2.cs
@@ -0,0 +1,22 @@
+﻿namespace Main.Solutions
+{
+    internal class Digits_2 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int say = 0;
+            if (n == 0) Console.WriteLine(1);
+            else
+            {
+                while (n != 0)
+                {
+                    n /= 10;
+                    say++;
+                }
+
+                Console.WriteLine(say);
+            }
+        }
+    }
+}
