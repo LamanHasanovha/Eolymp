0077a8cbb3bdbd812eee4a22d424d434c51fd325
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-19 18:27:39 +0400

    75. Pirates and coins

---
+++ Main/Solutions/PiratesAndCoins_75.cs
@@ -0,0 +1,20 @@
+﻿namespace Main.Solutions
+{
+    internal class PiratesAndCoins_75 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int a = int.Parse(tokens[0]);
+            int m = int.Parse(tokens[1]);
+            int n = 1;
+            while (m > 0)
+            {
+                m = m - a;
+                a++;
+                n++;
+            }
+            Console.WriteLine(n - 2);
+        }
+    }
+}
