7e34c7be28ef3f133c7a6d215f068f282e6c68e1
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-22 16:47:42 +0400

    905. What type of triangle?

---
+++ Main/Solutions/WhatTypeOfTriangle_905.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class WhatTypeOfTriangle_905 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int a = int.Parse(tokens[0]);
+            int b = int.Parse(tokens[1]);
+            int c = int.Parse(tokens[2]);
+
+            // b.t -> 1    b.y -> 2   m.t -> 3
+
+            if (a == b && b == c)
+            {
+                Console.WriteLine(1);
+            }
+            else if (a == b | a == c | b == c)
+            {
+                Console.WriteLine(2);
+            }
+            else
+            {
+                Console.WriteLine(3);
+            }
+        }
+    }
+}
