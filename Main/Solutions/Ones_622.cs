80fa3d8ece387c5883668ce45be109aa8553328c
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-22 16:11:09 +0400

    622.Ones

---
+++ Main/Solutions/Ones_622.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class Ones_622 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int n = int.Parse(tokens[0]);
+            int say = 0;
+            while (n != 0)
+            {
+
+                if (n % 2 != 0) say++;
+                n /= 2;
+            }
+            Console.WriteLine(say);
+
+        }
+    }
+}
