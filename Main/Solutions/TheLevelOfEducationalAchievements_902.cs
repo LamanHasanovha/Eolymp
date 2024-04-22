232c4d2e2d86e5d15b6820c351d2febdc6b00980
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-22 16:14:56 +0400

    902. The level of educational achievements

---
+++ Main/Solutions/TheLevelOfEducationalAchievements_902.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class TheLevelOfEducationalAchievements_902 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a <= 3)
+            {
+                Console.WriteLine("Initial");
+            }
+            else if (a <= 6)
+            {
+                Console.WriteLine("Average");
+            }
+            else if (a <= 9)
+            {
+                Console.WriteLine("Sufficient");
+            }
+            else Console.WriteLine("High");
+        }
+    }
+}
