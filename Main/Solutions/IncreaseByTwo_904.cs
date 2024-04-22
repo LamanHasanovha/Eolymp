5ab6accc1804710d88daf82f65b00418aded1d1f
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-22 16:46:08 +0400

    904. Increase by 2

---
+++ Main/Solutions/IncreaseByTwo_904.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class IncreaseByTwo_904 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = Array.ConvertAll
+                (Console.ReadLine().Split(), num => int.Parse(num));
+
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers[i] >= 0) numbers[i] += 2;
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));  // 1 2 3 4 
+        }
+    }
+}
