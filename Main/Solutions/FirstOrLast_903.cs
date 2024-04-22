900c7bb40fd36c3a8df2c1a06a97ba6a84c4c4c4
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-22 16:18:12 +0400

    903. First or last?

---
+++ Main/Solutions/FirstOrLast_903.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class FirstOrLast_903 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int abc = int.Parse(Console.ReadLine());
+
+            int a = abc / 100;
+            int c = abc % 10;
+
+            if (a > c) Console.WriteLine(a);
+            else if (c > a) Console.WriteLine(c);
+            else Console.WriteLine("=");
+        }
+    }
+}
