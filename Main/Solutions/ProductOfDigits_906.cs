bc398b0d7e54990f971baf160fd2c9257bf9f4e0
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-25 15:02:14 +0400

    906. Product of digits

---
+++ Main/Solutions/ProductOfDigits_906.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class ProductOfDigits_906 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 0) a *= -1;
+            int b = a % 10;
+            int c = a / 100;
+            int d = a / 10 % 10;
+            int cem = b * c * d;
+            Console.WriteLine(cem);
+        }
+    }
+}
