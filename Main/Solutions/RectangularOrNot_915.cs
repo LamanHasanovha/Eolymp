750ae18466359d6545a0a74980580d2305627133
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-29 14:50:32 +0400

    915.Rectangular or not?

---
+++ Main/Solutions/RectangularOrNot_915.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class RectangularOrNot_915 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int a = int.Parse(tokens[0]);
+            int b = int.Parse(tokens[1]);
+            int c = int.Parse(tokens[2]);
+
+            if ((b * b + c * c == a * a) | (c * c + a * a == b * b) | (a * a + b * b == c * c))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
