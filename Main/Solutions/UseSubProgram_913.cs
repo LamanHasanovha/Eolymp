f5f21adde38a057ad0c0537a5a67ac450f2afd3c
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-29 14:46:01 +0400

    913. Use sub-program

--- Main/Solutions/UseSubProgram_913.cs
+++ Main/Solutions/UseSubProgram_913.cs
@@ -19,19 +19,13 @@ namespace Main.Solutions
                 string token = Console.ReadLine();
                 matrix[i, 0] = double.Parse(token.Split()[0]);
                 matrix[i, 1] = double.Parse(token.Split()[1]);
-
-                //string[] tokens = Console.ReadLine().Split();
-                //for (int j = 0; j < m; j++)
-                //{
-                //    matrix[i, j] = int.Parse(tokens[j]);
-                //}
+   
             }
 
 
             for (int i = 0; i < n; i++)
             {
-                //Console.WriteLine
-                //    (matrix[i, 0] + matrix[i, 1] + " " + matrix[i, 0] * matrix[i, 1]);
+               
                 Console.WriteLine("{0:F4} {1:F4}",
                     matrix[i, 0] + matrix[i, 1], matrix[i, 0] * matrix[i, 1]);
             }
