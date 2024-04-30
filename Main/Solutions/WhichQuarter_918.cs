964da7085d1bace8fcf2e8e26c7a818446fa6700
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-30 20:39:46 +0400

    918. Which quarter?

---
+++ Main/Solutions/WhichQuarter_918.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class WhichQuarter_918 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+
+            string[] tokens = Console.ReadLine().Split();
+            double x = double.Parse(tokens[0]);
+            double y = double.Parse(tokens[1]);
+
+            if (x > 0 & y > 0)
+            {
+                Console.WriteLine(1);
+            }
+            else if (x < 0 & y > 0)
+            {
+                Console.WriteLine(2);
+            }
+            else if (x < 0 & y < 0)
+            {
+                Console.WriteLine(3);
+            }
+            else if (x == 0 | y == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(4);
+            }
+        }
+    }
+}
