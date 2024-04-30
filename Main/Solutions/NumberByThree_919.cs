7285341e060bfc90119c4256d88ec024ac0b77a8
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-30 20:41:29 +0400

    919. Number by 3

---
+++ Main/Solutions/NumberByThree_919.cs
@@ -0,0 +1,27 @@
+﻿namespace Main.Solutions
+{
+    internal class NumberByThree_919 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] array = Array.ConvertAll
+                (Console.ReadLine().Split(), r => double.Parse(r));
+
+            int count = 0;
+            double sum = 0;
+
+            for (int i = 2; i < n; i += 3)
+            {
+                if (array[i] > 0)
+                {
+                    count++;
+                    sum += array[i];
+                }
+            }
+
+            Console.WriteLine("{0} {1:N2}", count, sum);
+        }
+    }
+}
