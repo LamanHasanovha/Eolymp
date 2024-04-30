1712d043ad9c7079bf4a9e14d2987472bef1fb4f
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-04-30 20:37:33 +0400

    917. Twice the minimum

---
+++ Main/Solutions/TwiceTheMinimum_917.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class TwiceTheMinimum_917 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] tokens = Console.ReadLine().Split();
+
+            double[] numbers = new double[n];
+            double min = 100;
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = double.Parse(tokens[i]);
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            min *= 2;
+
+            Console.WriteLine("{0:N2}", min);
+        }
+    }
+}
