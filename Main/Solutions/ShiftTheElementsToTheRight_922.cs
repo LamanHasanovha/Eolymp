8d3eb85e5b52aac4c30675813694dc6034ed1cf3
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-05-02 19:54:32 +0400

    922. Shift the elements to the right

---
+++ Main/Solutions/ShiftTheElementsToTheRight_922.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Solutions
+{
+    internal class ShiftTheElementsToTheRight_922 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] tokens = Console.ReadLine().Split();
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(tokens[i]);
+            }
+
+            int[] changedNumbers = new int[n];
+            changedNumbers[0] = numbers[n - 1];
+
+            int iteration = 0;
+            for (int i = 1; i < n; i++, iteration++)
+            {
+                changedNumbers[i] = numbers[iteration];
+
+                Console.Write(changedNumbers[iteration] + " ");
+            }
+
+            Console.WriteLine(changedNumbers[n - 1]);
+        }
+    }
+}
