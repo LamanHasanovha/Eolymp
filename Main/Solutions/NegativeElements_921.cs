95e940bd89d4654456b60b484e50ae22b1fa19af
LamanHasanovha <laman.hasanovha.dev@gmail.com>
2024-05-02 19:53:00 +0400

    921. Negative elements

---
+++ Main/Solutions/NegativeElements_921.cs
@@ -0,0 +1,27 @@
+﻿namespace Main.Solutions
+{
+    internal class NegativeElements_921 : SolutionBase
+    {
+        internal override void Invoke()
+        {
+            int h = Convert.ToInt32(Console.ReadLine());
+            string[] eded = Console.ReadLine().Split();
+            decimal[] A = new decimal[eded.Length];
+            decimal count = 0;
+            decimal a = 0.00m;
+            for (int i = 0; i < eded.Length; i++)
+            {
+                A[i] = decimal.Parse(eded[i]);
+            }
+            foreach (var b in A)
+            {
+                if (b < 0)
+                {
+                    count++;
+                    a += b;
+                }
+            }
+            Console.WriteLine(count + " " + a);
+        }
+    }
+}
