63144d34ddca9fcff213908b2875b54fd02995a0
lamanHasanovha <asim.alizada@tayqatech.com>
2024-05-12 16:46:12 +0400

    924. Ring

---
+++ Main/Solutions/Ring_924.cs
@@ -0,0 +1,19 @@
+﻿namespace Main.Solutions;
+
+internal class Ring_924 : SolutionBase
+{
+    internal override void Invoke()
+    {
+        string[] tokens = Console.ReadLine().Split();
+        double hs = double.Parse(tokens[0]);
+        double r1 = double.Parse(tokens[1]);
+
+        double s = Math.Pow(r1, 2) * Math.PI;
+
+        double s2 = s - hs;
+
+        double r2 = Math.Sqrt(s2 / Math.PI);
+
+        Console.WriteLine("{0:N2}", r2);
+    }
+}
